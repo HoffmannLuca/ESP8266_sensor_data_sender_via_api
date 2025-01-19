5cca1191d0405973341f72dc1ebd0d97cf578b5b
HoffmannLuca <luca.hoffmann1000@gmail.com>
2025-01-19 14:47:46 +0100

    add index.html for local development only

---
+++ tailwind.config.js
@@ -0,0 +1,4 @@
+// just for Tailwind CSS IntelliSense inside index.html
+
+// Extension-ID: bradlc.vscode-tailwindcss
+// Extension-Link: https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss
